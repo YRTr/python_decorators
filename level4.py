1075c85ce2ff7c2d87ac667401c2db6e3aa66c26
yrtR <yrt2492@gmail.com>
2025-07-14 01:35:35 +0530

    wrap the decorator

---
+++ level4.py
@@ -0,0 +1,16 @@
+from functools import wraps
+
+def repeat(n):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            for _ in range(n):
+                func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+@repeat(3)
+def say_hi():
+    print('Hi!')
+
+say_hi()
