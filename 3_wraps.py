9c6a7adad6a4c1510f4f3b70f5292cc656a3a457
yrtR <yrt2492@gmail.com>
2025-08-07 23:56:36 +0530

    introducing wraps from functools module

---
+++ 3_wraps.py
@@ -0,0 +1,16 @@
+# Preserve Metadata with functools.wraps
+
+from functools import wraps
+def logger(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        print(f'Calling {func.__name__} with args:{args} kwargs:{kwargs}')
+        return func(*args, **kwargs)
+    return wrapper
+
+@logger
+def add(a,b):
+    return a+b
+
+add(3,4)
+print(add.__name__)
