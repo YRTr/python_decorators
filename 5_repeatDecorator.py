ca091996ffc329940514dc193e54924c047e828e
yrtR <yrt2492@gmail.com>
2025-08-08 00:01:33 +0530

    Decorator factory

---
+++ 5_repeatDecorator.py
@@ -0,0 +1,18 @@
+#Decorator with Arguments (Decorator Factory)
+
+from functools import wraps
+
+def repeat(n):
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            for _ in range(n):
+                func(*args, **kwargs)
+        return wrapper
+    return decorator
+
+@repeat(3)
+def say_hi():
+    print('Hi!')
+
+say_hi()
